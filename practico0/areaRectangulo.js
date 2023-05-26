ee5f0779e91871b0660f772eef8d94a7d8085e85
gitdec <dec.breaking@gmail.com>
2023-05-26 04:40:08 -0300

    Agrego el archivo conversionGrados.js

--- practico0/areaRectangulo.js
+++
@@ -1,7 +0,0 @@
-function arearec (num1, num2) {
-    let area = num1 * num2
-    console.log("El área de", num1,"x ", num2,"es = ", area)
-}
-arearec(5, 3); //El area a) es: 15
-arearec(12.5, 5.3); //El area b) es: 66.25
-
--- practico0/promedio3.js
+++
@@ -1,8 +0,0 @@
-function calcularPromedio(num1, num2, num3) {
-    let promedio = (num1 + num2 + num3) / 3;
-    console.log("El promedio es: " + promedio);
-  }
-  
-  calcularPromedio(5, 7, 9); // Promedio a) 7
-  calcularPromedio(-2, -3, -8); // Promedio b) -4.33
-  calcularPromedio(-1.2, 5.3, -3.3); // Promedio c) 0.26
