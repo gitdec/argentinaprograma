5c8a89bbeea14b88438decc8da9eb04993436b44
gitdec <dec.breaking@gmail.com>
2023-06-01 19:57:47 -0300

    AgregueDetalles

--- practico_0b/promedio6.js
+++
@@ -1,6 +0,0 @@
-function calcularPromedio(num1, num2, num3, num4, num5, num6) {
-    let promedio = (num1 + num2 + num3 + num4 + num5 + num6) / 3;
-    console.log("El promedio es: " + promedio);
-  }
-  
-  calcularPromedio(4, 7, 5, 3, -4, 1.85); // Promedio 6 numeros: 
